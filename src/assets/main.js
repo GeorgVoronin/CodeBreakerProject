ca65160010c01e0bafc3458e37fb58825b9b7037
GeorgVoronin <35695567+GeorgVoronin@users.noreply.github.com>
2018-01-23 14:53:49 +0100

    Step8.5

--- src/assets/main.js
+++ src/assets/main.js
@@ -13,9 +13,9 @@ function guess() {
 //implement new functions here
 
 function setHiddenFields(){
-  attempt = 0;
-  answer = Math.floor(Math.random()*10000);
-  answer = answer.toString();
+  attempt.value = 0;
+  answer.value = Math.floor(Math.random()*10000);
+  answer. = answer.toString();
 
   while (answer.length<4) {answer = '0' + answer;}
 }
