37c74d3e208d67920127a6cc7ecbbfb104a014ba
GeorgVoronin <35695567+GeorgVoronin@users.noreply.github.com>
2018-01-23 15:59:53 +0100

    Step12

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,25 +4,37 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
-    if(answer==='' && attempt==='') {setHiddenFields();}
+    if(answer==='' || attempt==='') {setHiddenFields();}
 
-    if (validateInput(input.value)) {attempt++;} else {return;}
+    if (validateInput(input.value)) {attempt.value++;} else {return;}
 }
 
 //implement new functions here
 
 function setHiddenFields(){
-  attempt.value = 0;
-  answer.value = Math.floor(Math.random()*10000);
-  answer.toString();
-  while (answer.length<4) {answer = '0' + answer;}
+  attempt.value = '0';
+  answer.value = Math.floor(Math.random()*10000).toString();
+  while (answer.value.length<4) {answer.value = '0' + answer.value;}
 }
 
 function setMessage(a){
-  let message = document.getElementById('message');
-  message.innerHTML=a;
+  document.getElementById('message').innerHTML=a;
 }
 
 function validateInput(a){
   if (a.length===4) {return true;} else {setMessage("Guesses must be exactly 4 characters long."); return false;}
+}
+
+function getResults(a){
+  let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+  for (var i = 0; i < a.length; i++){
+    if (a.CharAt(i)===answer.value.CharAt(i)) {html += '<span class="glyphicon glyphicon-ok"></span>';}
+    else if (answer.value.includes(a.CharAt(i)) && !a.CharAt(i)===answer.value.CharAt(i)) {html += '<span class="glyphicon glyphicon-transfer"></span>';}
+    else {html += '<span class="glyphicon glyphicon-remove"></span>';}
+  }
+  html += '</div></div>';
+
+  document.getElementById('results').innerHTML+=html;
+
+
 }
